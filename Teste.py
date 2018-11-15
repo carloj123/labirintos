658210c7913603a25b90ff35c464efd37ec2ac31
carlo José Gallicchio Laitano <carlojose@MacBook-Air-de-carlo.local>
2018-11-15 04:16:17 -0200

    Ajuste na estrutura do Código

--- Teste.py
+++ Teste.py
@@ -9,6 +9,142 @@ import sys
 sys.setrecursionlimit(1500)
 
 lista = list()
+def escreveLinha(x1,y1,x2,y2,arquivo):
+        arquivo.write("\t\t\t<polyline points=\"%d,%d %d,%d\" />\n" % (x1,y1,x2,y2))
+
+def escreveCirculo(x, y,arquivo,metragem):
+    metragem = metragem
+    arquivo.write("\t\t\t<circle cx=\"%.1f\" cy=\"%.1f\" r=\"0.2\" stroke=\"red\" fill=\"red\" />\n" % (x+0.5,y+0.5))
+
+    l = 0
+    c = 0
+    #lista = ['1001','1010','1010','0110',"0101",'1001','1010','1100','0101','0101','1101','0111','0011','0010','0010','1100']
+
+    for i in range(0,len(lista)):
+        palavra = lista[i]
+        
+        if palavra[0] == '1':
+            escreveLinha(c, l, c+1, l,arquivo)
+        if palavra[1] == '1':
+            escreveLinha(c+1, l, c+1, l+1,arquivo)
+        if palavra[2] == '1':
+            escreveLinha(c, l+1, c+1, l+1,arquivo)
+        if palavra[3] == '1':
+            escreveLinha(c, l, c, l+1,arquivo)
+
+        
+
+        c += 1
+        if c == int(metragem[1]):
+            c = 0
+            l += 1
+
+def arestas(tamanho_linha,inicio,fim):
+    global lista,foras,arestar_time
+
+    tamanho_linha = tamanho_linha
+    saidas = list()
+    saida = ''
+    i = 0
+    while i < len(lista):
+        if i == inicio:
+            saidas.append(str(i)+','+'noneA')
+            i += 1
+            if i>= len(lista):
+                print('Arestas feitas')
+                return saidas
+
+        if i == fim:
+            saidas.append(str(i)+','+'noneB')
+            i += 1
+            if i>= len(lista):
+                print('Arestas feitas')
+                return saidas
+
+        if lista[i][0] == '0':
+            ind = i
+            saida = str(ind)+','+str(ind-tamanho_linha)
+            saidas.append(saida)
+
+
+        if lista[i][1] == '0':
+            ind = i
+            saida = str(ind)+','+str(ind+1)
+            saidas.append(saida)
+
+        if lista[i][2] == '0':
+            ind = i
+            saida = str(ind)+','+str(ind+tamanho_linha)
+            saidas.append(saida)   
+        if lista[i][3] == '0':
+            ind = i
+            saida = str(ind)+','+str(ind-1)
+            saidas.append(saida) 
+        i += 1
+
+    print('Arestas feitas')
+    return saidas
+
+
+def mostra(lista,tamanho_linha):
+    entradas= set()
+    i = 0
+
+    while i < tamanho_linha:
+        try:
+            k = lista[i]
+        except:
+            print(lista)
+            print(i)
+            exit()
+        if i>= 0 and i<tamanho_linha:
+            if i == 0:
+                if k[0] == '0' or k[3] == '0':
+                    entradas.add(i)
+
+            elif i == tamanho_linha-1:
+                if k[0] == '0' or k[1] == '0':
+                    entradas.add(i)
+
+            elif k[0] == '0':
+                entradas.add(i)
+        i += 1
+    while i < len(lista):
+        k = lista[i]
+
+        if i > (math.pow(tamanho_linha,2) - tamanho_linha) and i < math.pow(tamanho_linha,2):
+            if i == (math.pow(tamanho_linha,2)-1) - tamanho_linha:
+                if k[2] == '0' or k[3] == '0':
+                    entradas.add(i)
+
+            elif i == math.pow(tamanho_linha,2)-1:
+                if k[2] == '0' or k[1] == '0':
+                    entradas.add(i)
+
+            elif k[2] == '0':
+                entradas.add(i)
+        i += 1
+
+    if len(entradas) <= 1:
+        j = tamanho_linha-1
+        while j < math.pow(tamanho_linha,2):
+            if lista[j][1] == '0':
+                entradas.add(j)
+
+            j += tamanho_linha
+
+    if len(entradas) <= 1:
+        j = 0
+        while j < math.pow(tamanho_linha, 2):
+            if lista[j][3] == '0':
+                entradas.add(j)
+            j += tamanho_linha
+
+
+
+    print(entradas)
+    return entradas
+
 def main():
     arquivos = ['25','50','75','100','150','200','250','300','400','500']
     numero_arqivo = input('Favor digite o numero do arquivo ')
@@ -46,141 +182,7 @@ def main():
     fr.write("\t<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"%scm\" height=\"%scm\" viewBox=\"-0.1 -0.1 %s.2 %s.2\">\n" % (metragem[0], metragem[1], metragem[0], metragem[1]))
     fr.write("\t\t<g style=\"stroke-width:.1; stroke:black; stroke-linejoin:miter; stroke-linecap:butt;\">\n")
 
-    def escreveLinha(x1,y1,x2,y2):
-        fr.write("\t\t\t<polyline points=\"%d,%d %d,%d\" />\n" % (x1,y1,x2,y2))
-
-    def escreveCirculo(x, y,arquivo):
-        fr.write("\t\t\t<circle cx=\"%.1f\" cy=\"%.1f\" r=\"0.2\" stroke=\"red\" fill=\"red\" />\n" % (x+0.5,y+0.5))
-
-    l = 0
-    c = 0
-    #lista = ['1001','1010','1010','0110',"0101",'1001','1010','1100','0101','0101','1101','0111','0011','0010','0010','1100']
-
-    for i in range(0,len(lista)):
-        palavra = lista[i]
-        
-        if palavra[0] == '1':
-            escreveLinha(c, l, c+1, l)
-        if palavra[1] == '1':
-            escreveLinha(c+1, l, c+1, l+1)
-        if palavra[2] == '1':
-            escreveLinha(c, l+1, c+1, l+1)
-        if palavra[3] == '1':
-            escreveLinha(c, l, c, l+1)
-
-        
-
-        c += 1
-        if c == int(metragem[1]):
-            c = 0
-            l += 1
-
-    def arestas(tamanho_linha,inicio,fim):
-        global lista,foras,arestar_time
-
-        tamanho_linha = tamanho_linha
-        saidas = list()
-        saida = ''
-        i = 0
-        while i < len(lista):
-            if i == inicio:
-                saidas.append(str(i)+','+'noneA')
-                i += 1
-                if i>= len(lista):
-                    print('Arestas feitas')
-                    return saidas
-
-            if i == fim:
-                saidas.append(str(i)+','+'noneB')
-                i += 1
-                if i>= len(lista):
-                    print('Arestas feitas')
-                    return saidas
-
-            if lista[i][0] == '0':
-                ind = i
-                saida = str(ind)+','+str(ind-tamanho_linha)
-                saidas.append(saida)
-
-
-            if lista[i][1] == '0':
-                ind = i
-                saida = str(ind)+','+str(ind+1)
-                saidas.append(saida)
-
-            if lista[i][2] == '0':
-                ind = i
-                saida = str(ind)+','+str(ind+tamanho_linha)
-                saidas.append(saida)   
-            if lista[i][3] == '0':
-                ind = i
-                saida = str(ind)+','+str(ind-1)
-                saidas.append(saida) 
-            i += 1
-
-        print('Arestas feitas')
-        return saidas
-
-
-    def mostra(lista,tamanho_linha):
-        entradas= set()
-        i = 0
-
-        while i < tamanho_linha:
-            try:
-                k = lista[i]
-            except:
-                print(lista)
-                print(i)
-                exit()
-            if i>= 0 and i<tamanho_linha:
-                if i == 0:
-                    if k[0] == '0' or k[3] == '0':
-                        entradas.add(i)
-
-                elif i == tamanho_linha-1:
-                    if k[0] == '0' or k[1] == '0':
-                        entradas.add(i)
-
-                elif k[0] == '0':
-                    entradas.add(i)
-            i += 1
-        while i < len(lista):
-            k = lista[i]
-
-            if i > (math.pow(tamanho_linha,2) - tamanho_linha) and i < math.pow(tamanho_linha,2):
-                if i == (math.pow(tamanho_linha,2)-1) - tamanho_linha:
-                    if k[2] == '0' or k[3] == '0':
-                        entradas.add(i)
-
-                elif i == math.pow(tamanho_linha,2)-1:
-                    if k[2] == '0' or k[1] == '0':
-                        entradas.add(i)
-
-                elif k[2] == '0':
-                    entradas.add(i)
-            i += 1
-
-        if len(entradas) <= 1:
-            j = tamanho_linha-1
-            while j < math.pow(tamanho_linha,2):
-                if lista[j][1] == '0':
-                    entradas.add(j)
-
-                j += tamanho_linha
-
-        if len(entradas) <= 1:
-            j = 0
-            while j < math.pow(tamanho_linha, 2):
-                if lista[j][3] == '0':
-                    entradas.add(j)
-                j += tamanho_linha
-
-
-
-        print(entradas)
-        return entradas
-
+    
 
 
     ##############################
@@ -236,13 +238,13 @@ def main():
         palavra = lista[i]
         
         if palavra[0] == '1':
-            escreveLinha(c, l, c+1, l)
+            escreveLinha(c, l, c+1, l,fr)
         if palavra[1] == '1':
-            escreveLinha(c+1, l, c+1, l+1)
+            escreveLinha(c+1, l, c+1, l+1,fr)
         if palavra[2] == '1':
-            escreveLinha(c, l+1, c+1, l+1)
+            escreveLinha(c, l+1, c+1, l+1,fr)
         if palavra[3] == '1':
-            escreveLinha(c, l, c, l+1)
+            escreveLinha(c, l, c, l+1,fr)
 
         c += 1
         if c == int(metragem[1]):
@@ -261,7 +263,7 @@ def main():
             lg += 1
         j = str(i)
         if j in g.fila:
-            escreveCirculo(cg, lg,fr)
+            escreveCirculo(cg, lg,fr,metragem)
 
         cg += 1
         i += 1
